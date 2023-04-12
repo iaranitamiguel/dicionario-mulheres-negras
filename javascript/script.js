a9c0e62ff9b1ad053722c45cb0894353b8aca166
Iaranita Miguel Reis <76664737+iaranitamiguel@users.noreply.github.com>
2023-04-12 20:44:40 -0300

    Tornando o site mais responsivo
    Usando @media para dar responsividade ao site

---
+++ javascript/script.js
@@ -0,0 +1,11 @@
+function nomesMulheres (){
+    var mulheresNegras = [" Marielle Franco",
+"Carolina de Jesus",
+"Rosa Parks",
+"Angela Davis",
+"Conceição Evaristo"
+];
+alert(mulheresNegras);
+}
+
+nomesMulheres();
